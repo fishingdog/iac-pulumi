be07ae96d255119df4f5a9a35b8351aaf06dedae
Beluga_samsung <2x3tech@gmail.com>
2023-10-11 16:41:57 -0700

    pre-done

--- src/main/java/myproject/Infrastructure.java
+++ src/main/java/myproject/Infrastructure.java
@@ -1,27 +1,52 @@
 package myproject;
 
 import com.pulumi.Context;
+import com.pulumi.aws.AwsFunctions;
 import com.pulumi.aws.ec2.*;
 import com.pulumi.aws.ec2.inputs.RouteTableRouteArgs;
+import com.pulumi.aws.inputs.GetAvailabilityZonesArgs;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Map;
+
 
 public class Infrastructure {
     public static void deploy(Context ctx) {
-        Vpc myvpc = createVpc();
-        InternetGateway igw = createInternetGateway();
+        String vpcCidrBlockValue = System.getenv("VPC_CIDR_BLOCK");
+        String vpcInstanceTenancyValue = System.getenv("VPC_INSTANCE_TENANCY");
+        String vpcName = System.getenv("VPC_TAG_NAME");
+        Vpc myvpc = createVpc(vpcCidrBlockValue, vpcInstanceTenancyValue, vpcName);
+
+        String igTagNameValue = System.getenv("VPC_TAG_NAME");
+        InternetGateway igw = createInternetGateway(igTagNameValue);
+
         attachInternetGateway(myvpc, igw);
-        Subnet mysubnet = createSubnet(myvpc);
-        RouteTable pubRT = createRouteTable(myvpc, igw);
-        RouteTable privRT = createPrivateRouteTable(myvpc);
-        associateRouteTableToSubnet(mysubnet, pubRT);
+
+        String routeCidrBlockValue = System.getenv("RT_ROUTE_CIDR_BLOCK");
+        String routePublicTableNameValue = System.getenv("RT_PUBLIC_ROUTE_TABLE_NAME");
+        RouteTable pubRT = createRouteTable(myvpc, igw, routeCidrBlockValue, routePublicTableNameValue);
+
+        String routePrivateTableNameValue = System.getenv("RT_PRIVATE_ROUTE_TABLE_NAME");
+        RouteTable privRT = createPrivateRouteTable(myvpc, routePrivateTableNameValue);
+
+//        String subnetCiderPrefix = System.getenv("PUBLIC_SUBNET_CIDER_PREFIX");
+//        String subnetCiderStartingIndex = System.getenv("PUBLIC_SUBNET_CIDER_START");
+//        String subnetTagNamePrefix = System.getenv("PUBLIC_SUBNET_TAG_NAME_PREFIX");
+//        String numOfSubnets = System.getenv("NUM_OF_PUBLIC_SUBNETS");
+        String subnetCiderPrefix = "10.1.";
+        String subnetCiderStartingIndex = "0";
+        String subnetTagNamePrefix = "public_subnet";
+        String numOfSubnets = "3";
+        createMultipleSubnetWithRouteTable(myvpc, subnetCiderPrefix, subnetCiderStartingIndex, subnetTagNamePrefix, numOfSubnets, pubRT);
+
+        subnetCiderStartingIndex = "100";
+        subnetTagNamePrefix = "private_Subnet";
+        createMultipleSubnetWithRouteTable(myvpc, subnetCiderPrefix, subnetCiderStartingIndex, subnetTagNamePrefix, numOfSubnets, privRT);
 
     }
 
-    private static Vpc createVpc() {
-        String cidrBlockValue = System.getenv("CIDR_BLOCK");
-        String instanceTenancyValue = System.getenv("INSTANCE_TENANCY");
-        String tagNameValue = System.getenv("TAG_NAME");
+    private static Vpc createVpc(String cidrBlockValue, String instanceTenancyValue, String tagNameValue) {
 
         return new Vpc("main", VpcArgs.builder()
                 .cidrBlock(cidrBlockValue != null ? cidrBlockValue : "10.1.0.0/16")
@@ -30,8 +55,7 @@ public class Infrastructure {
                 .build());
     }
 
-    private static InternetGateway createInternetGateway() {
-        String tagNameValue = System.getenv("TAG_NAME");
+    private static InternetGateway createInternetGateway(String tagNameValue) {
         return new InternetGateway("gw", InternetGatewayArgs.builder()
                 .tags(Map.of("Name", tagNameValue != null ? tagNameValue : "defaultGW"))
                 .build());
@@ -44,22 +68,48 @@ public class Infrastructure {
                 .build());
     }
 
-    public static Subnet createSubnet(Vpc myvpc) {
-        String subnetCiderBlockValue = System.getenv("SUBNET_CIDR_BLOCK");
-        String subnetTagNameValue = System.getenv("SUBNET_TAG_NAME");
-
+    public static Subnet createSubnet(Vpc myvpc, String subnetCiderBlockValue, String myZone, String subnetTagNameValue) {
         return new Subnet("main", SubnetArgs.builder()
                 .vpcId(myvpc.id())
                 .cidrBlock(subnetCiderBlockValue != null ? subnetCiderBlockValue : "10.1.1.0/24")
                 .tags(Map.of("Name", subnetTagNameValue != null ? subnetTagNameValue : "mysubnet"))
+                .availabilityZone(myZone != null ? myZone : "us-west-2a")
                 .build()
             );
     }
 
-    private static RouteTable createRouteTable(Vpc myvpc, InternetGateway igw) {
-        String routeCidrBlockValue = System.getenv("ROUTE_CIDR_BLOCK");
-        String routeTableNameValue = System.getenv("PUBLIC_ROUTE_TABLE_NAME");
+    public static void createMultipleSubnetWithRouteTable(Vpc myvpc, String subnetCiderBlockPrefix, String subnetCiderStartingIndex, String subnetTagNamePrefix, String numOfSubnets, RouteTable rt) {
 
+        if (subnetCiderBlockPrefix == null) { subnetCiderBlockPrefix = "10.1."; }
+        int startIndex = 0;
+        if (subnetCiderStartingIndex != null) {startIndex = Integer.parseInt(subnetCiderStartingIndex);}
+        if (subnetTagNamePrefix == null) { subnetTagNamePrefix = "mysubnet"; }
+        int numOfSubnet = 0;
+        if (numOfSubnets == null) {
+            numOfSubnet = 3;
+        } else { numOfSubnet = Integer.parseInt(numOfSubnets); }
+
+        final var available = AwsFunctions.getAvailabilityZones(GetAvailabilityZonesArgs.builder()
+                .state("available")
+                .build());
+
+        for (int i = 0; i < numOfSubnet; i++) {
+            int curIndex = startIndex + i;
+            String subnetCiderBlockValue = subnetCiderBlockPrefix + curIndex + ".0/24";
+            String subnetTagNameValue = subnetTagNamePrefix + i;
+            final int zoneIndex = i;
+            Subnet mysubnet = new Subnet(subnetTagNameValue, SubnetArgs.builder()
+                    .tags(Map.of("Name", subnetTagNameValue))
+                    .vpcId(myvpc.id())
+                    .cidrBlock(subnetCiderBlockValue)
+                    .availabilityZone(available.applyValue(getAvailabilityZonesResult -> getAvailabilityZonesResult.names().get(zoneIndex)))
+                    .build());
+            associateRouteTableToSubnet(mysubnet, rt);
+
+        }
+    }
+
+    private static RouteTable createRouteTable(Vpc myvpc, InternetGateway igw, String routeCidrBlockValue, String routeTableNameValue) {
         return new RouteTable("testRouteTable", RouteTableArgs.builder()
                 .vpcId(myvpc.id())
                 .routes(RouteTableRouteArgs.builder()
@@ -70,25 +120,35 @@ public class Infrastructure {
                 .build());
     }
 
-    private static RouteTable createPrivateRouteTable(Vpc myvpc) {
-        String privateRouteTableNameValue = System.getenv("PRIVATE_ROUTE_TABLE_NAME");
+    private static RouteTable createPrivateRouteTable(Vpc myvpc, String routePrivateTableNameValue) {
 
         return new RouteTable("privateRouteTable", RouteTableArgs.builder()
                 .vpcId(myvpc.id())
-                .tags(Map.of("Name", privateRouteTableNameValue != null ? privateRouteTableNameValue : "default_private_route_table"))
+                .tags(Map.of("Name", routePrivateTableNameValue != null ? routePrivateTableNameValue : "default_private_route_table"))
                 .build());
     }
 
-    private static RouteTableAssociation associateRouteTableToSubnet(Subnet mysubnet, RouteTable pubRT) {
-        String associationName = System.getenv("ROUTE_TABLE_ASSOCIATION_NAME");
-
-        return new RouteTableAssociation(
-                associationName != null ? associationName : "routeTableAssociation",
-                RouteTableAssociationArgs.builder()
-                        .subnetId(mysubnet.id())
-                        .routeTableId(pubRT.id())
-                        .build()
+    private static void associateRouteTableToSubnet(Subnet mysubnet, RouteTable rt) {
+        String randomName = generateRandomString(10);
+        new RouteTableAssociation(randomName, RouteTableAssociationArgs.builder()
+                    .subnetId(mysubnet.id())
+                    .routeTableId(rt.id())
+                    .build()
         );
     }
+
+
+    public static String generateRandomString(int length) {
+        final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            sb.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+        }
+
+        return sb.toString();
+    }
+
 
 }
