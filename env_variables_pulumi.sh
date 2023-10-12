877146d2341a5fd1f38cd726eb508fb656012556
Beluga_samsung <2x3tech@gmail.com>
2023-10-12 13:38:20 -0700

    making changes to env_var take in

--- env_variables_pulumi.sh
+++ env_variables_pulumi.sh
@@ -16,21 +16,32 @@ export RT_PUBLIC_ROUTE_TABLE_NAME="null"
 #"default_pub_route_table"
 export RT_PRIVATE_ROUTE_TABLE_NAME="null"
 #"default_private_route_table"
-export PUBLIC_SUBNET_CIDER_PREFIX="null"
+
+export PUBLIC_SUBNET_CIDER_LIST = "10.1.15.0/24, 10.1.17.0/24, 10.1.19.0/24"
+export PUBLIC_SUBNET_TAG_NAME_LIST = "public_subnet_aria, public_subnet_bria, public_subnet_cria"
+
+export PRIV_SUBNET_CIDER_LIST = "10.1.205.0/24, 10.1.207.0/24, 10.1.209.0/24"
+export PRIV_SUBNET_TAG_NAME_LIST = "private_subnet_aron, private_subnet_bron, private_subnet_cron"
+
+
+
+
+
+#export PUBLIC_SUBNET_CIDER_PREFIX="null"
 #"10.1."
-export PUBLIC_SUBNET_CIDER_START="4"
+#export PUBLIC_SUBNET_CIDER_START="4"
 #"0"
-export PUBLIC_SUBNET_TAG_NAME_PREFIX="null"
+#export PUBLIC_SUBNET_TAG_NAME_PREFIX="null"
 #"public_subnet"
-export NUM_OF_PUBLIC_SUBNETS="null"
+#export NUM_OF_PUBLIC_SUBNETS="null"
 #"3"
-export PRIV_SUBNET_CIDER_PREFIX="null"
+#export PRIV_SUBNET_CIDER_PREFIX="null"
 #"10.1."
-export PRIV_SUBNET_CIDER_START="null"
+#export PRIV_SUBNET_CIDER_START="null"
 #"100"
-export PRIV_SUBNET_TAG_NAME_PREFIX="null"
-"private_subnet"
-export NUM_OF_PUBLIC_SUBNETS_PRIV="null"
+#export PRIV_SUBNET_TAG_NAME_PREFIX="null"
+#"private_subnet"
+#export NUM_OF_PUBLIC_SUBNETS_PRIV="null"
 #"3"
 
 # to change aws account to demo:
