88509d1dcfe6dd6749ae8cb7c719a04e85e2afb7
Yan Wu <113943930+YanWuGit@users.noreply.github.com>
2023-10-19 18:34:54 -0700

    Branch (#10)
    * how to attach security group to ec2?
    
    * complete
    
    ---------
    
    Co-authored-by: Beluga_samsung <2x3tech@gmail.com>

--- src/main/java/myproject/CreateEC2Instance.java
+++ src/main/java/myproject/CreateEC2Instance.java
@@ -1,28 +1,20 @@
 package myproject;
 
-import com.pulumi.Context;
-import com.pulumi.Pulumi;
+
 import com.pulumi.aws.ec2.*;
 import com.pulumi.aws.ec2.inputs.InstanceRootBlockDeviceArgs;
-import com.pulumi.core.Output;
-import com.pulumi.aws.ec2.inputs.GetAmiArgs;
-
-import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class CreateEC2Instance {
 
     public static Instance createEC2Instance(SecurityGroup sg, String ami, Subnet mysubnet) {
-        List<String> sglist = new ArrayList<>();
         return new Instance("MyEc2Instance", InstanceArgs.builder()
                 .ami(ami)
                 .instanceType("t2.micro")
                 .subnetId(mysubnet.id())
+                .vpcSecurityGroupIds(sg.id().applyValue(List::of))
+                .keyName("testA5")
                 .rootBlockDevice(InstanceRootBlockDeviceArgs.builder()
                         .volumeType("gp2")
                         .volumeSize(25)
