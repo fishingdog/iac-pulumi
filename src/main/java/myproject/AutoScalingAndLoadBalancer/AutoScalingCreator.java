5f4a52e6fcdbe006840610f841b54022c35eb54a
Beluga_samsung <2x3tech@gmail.com>
2023-11-15 22:05:06 -0800

    complete

--- src/main/java/myproject/AutoScalingAndLoadBalancer/AutoScalingCreator.java
+++ src/main/java/myproject/AutoScalingAndLoadBalancer/AutoScalingCreator.java
@@ -6,13 +6,15 @@ import com.pulumi.aws.autoscaling.GroupArgs;
 import com.pulumi.aws.autoscaling.Policy;
 import com.pulumi.aws.autoscaling.PolicyArgs;
 import com.pulumi.aws.autoscaling.inputs.GroupLaunchTemplateArgs;
+import com.pulumi.aws.autoscaling.inputs.PolicyTargetTrackingConfigurationArgs;
+import com.pulumi.aws.autoscaling.inputs.PolicyTargetTrackingConfigurationPredefinedMetricSpecificationArgs;
 import com.pulumi.aws.cloudwatch.MetricAlarm;
 import com.pulumi.aws.cloudwatch.MetricAlarmArgs;
 import com.pulumi.aws.ec2.LaunchTemplate;
 import com.pulumi.core.Output;
 
 import java.util.List;
-
+import java.util.Map;
 
 
 public class AutoScalingCreator {
@@ -23,6 +25,7 @@ public class AutoScalingCreator {
 //                .availabilityZones("us-west-2b")
                 .maxSize(3)
                 .minSize(1)
+                .desiredCapacity(1)
                 .vpcZoneIdentifiers(subnetIds)
                 .launchTemplate(GroupLaunchTemplateArgs.builder()
                         .id(launchTemplate.id())
@@ -41,6 +44,7 @@ public class AutoScalingCreator {
 
     public static Policy createAutoScalingUpPolicy(Group group) {
         return new Policy("scaleUpPolicy", PolicyArgs.builder()
+                .policyType("SimpleScaling")
                 .adjustmentType("ChangeInCapacity")
                 .scalingAdjustment(1)
                 .cooldown(60)
@@ -48,15 +52,16 @@ public class AutoScalingCreator {
                 .build());
     }
 
-    public static MetricAlarm createAutoScalingUpMetricAlarm(Policy policy) {
+    public static MetricAlarm createAutoScalingUpMetricAlarm(Policy policy, Group group) {
         return new MetricAlarm("cpuHighAlarm", MetricAlarmArgs.builder()
                 .comparisonOperator("GreaterThanThreshold")
-                .evaluationPeriods(2)
+                .evaluationPeriods(1)
                 .metricName("CPUUtilization")
                 .namespace("AWS/EC2")
-                .period(60)
+                .period(180)
                 .statistic("Average")
                 .threshold(5.0)
+                .dimensions(group.name().applyValue(name -> Map.of("AutoScalingGroupName", name)))
                 .alarmActions(policy.arn().applyValue(List::of))
                 .build());
     }
@@ -70,15 +75,16 @@ public class AutoScalingCreator {
                 .build());
     }
 
-    public static MetricAlarm createAutoScalingDownMetricAlarm(Policy policy) {
+    public static MetricAlarm createAutoScalingDownMetricAlarm(Policy policy, Group group) {
         return new MetricAlarm("cpuLowAlarm", MetricAlarmArgs.builder()
                 .comparisonOperator("LessThanThreshold")
-                .evaluationPeriods(2)
+                .evaluationPeriods(1)
                 .metricName("CPUUtilization")
                 .namespace("AWS/EC2")
-                .period(60)
+                .period(180)
                 .statistic("Average")
                 .threshold(3.0)
+                .dimensions(group.name().applyValue(name -> Map.of("AutoScalingGroupName", name)))
                 .alarmActions(policy.arn().applyValue(List::of))
                 .build());
     }
